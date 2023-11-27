f1b5352ee750416dc195f4972e8ea9d44891bf43
Donato-Carola <don94@live.it>
2023-11-27 17:23:51 +0100

    inserito js per creare i singoli img

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ l'immagine grande in modo da poter stilare lo slider.
 Milestone 1:
 Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
 Al click dell'utente sulle frecce verso l'alto o verso il basso, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+
 Milestone 2: Aggiungere il ciclo infinito del carosello.
 Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso l'alto, la miniatura che deve attivarsi sarà 
 l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso il basso.*/
@@ -43,6 +44,15 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+
+
+
+const aside = document.querySelector('aside')
+
+for (let i=0; i<images.length; i++){
+    aside.innerHTML +=`<div class="col-12 p-0"><img src='${images[i].image}'> </div>`
+}
 
 
 
